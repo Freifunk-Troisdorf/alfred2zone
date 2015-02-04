a036994755d73e312a0c02eaef0361c275c3ff76
root <root@map.freifunk-troisdorf.de>
2015-02-04 19:05:07 +0100

    Tdf Changes

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export LC_ALL=de_DE.UTF-8
 cd /etc/bind/alfred2zone
-curl -s map.freifunk-troisdorf.de/alfred_merged.json | /usr/bin/python3 alfred2zone.py > /etc/bind/fftdf/db.nodes.fftdf
+curl -s map.infra.fftdf/alfred_merged.json | /usr/bin/python3 alfred2zone.py > /etc/bind/fftdf/db.nodes.fftdf
 /usr/sbin/rndc reload
