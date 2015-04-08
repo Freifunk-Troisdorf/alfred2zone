d5da2a4ebcc6c7736c584cbc50e4c1115f356711
stebifan <stefan@tro-mail.de>
2015-04-08 13:59:19 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export LC_ALL=de_DE.UTF-8
-cd /etc/bind/alfred2zone
-curl -s map.infra.fftdf/alfred_merged.json | /usr/bin/python3 alfred2zone.py > /etc/bind/fftdf/db.nodes.fftdf
-/usr/sbin/rndc reload
+cd /srv/alfred2zone
+cat /srv/ffmap-d3/build/alfred_merged.json | /usr/bin/python3 alfred2zone.py > /srv/ffmap-d3/build/db.nodes.fftdf
